90132c1faf5faa592628a2574c11ab9ddb32d06b
nichita <nichita@cadvegra.com>
2017-11-10 16:21:12 +0000

    added testfx

---
+++ src/test/java/calculatortest/TestPlus.java
@@ -0,0 +1,24 @@
+package calculatortest;
+
+import javafx.scene.control.TextField;
+import org.junit.Test;
+import org.testfx.util.WaitForAsyncUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestPlus extends TestBase {
+    @Test
+    public void plusTest() throws Exception{
+//        String eight = "8";
+//        String five = "5";
+        clickOn("8");
+        clickOn("+");
+        clickOn("5");
+        clickOn("=");
+
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertEquals(((TextField)find("#textf")).getText(), "13");
+    }
+
+}
