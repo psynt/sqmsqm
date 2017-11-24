2c89047267100622e2990a90fcb0160632cb5519
Kesava <psyku@nottingham.ac.uk>
2017-11-24 14:42:32 +0000

    Added minus test

---
+++ src/test/java/calculatortest/TestMinus.java
@@ -0,0 +1,23 @@
+package calculatortest;
+
+
+import javafx.scene.control.TextField;
+import org.junit.Test;
+import org.testfx.util.WaitForAsyncUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMinus extends TestBase {
+    @Test
+    public void plusTest() {
+        clickOn("8");
+        clickOn("-");
+        clickOn("5");
+        clickOn("=");
+
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertEquals(((TextField)find("#textf")).getText(), "3");
+    }
+
+}
