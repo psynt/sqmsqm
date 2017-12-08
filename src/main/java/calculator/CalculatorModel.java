f5cd1a474a021135836f8f6fd76ff8d2d80a647f
nichita <nichita@cadvegra.com>
2017-12-08 16:44:25 +0000

    added init values to model

--- src/main/java/calculator/CalculatorModel.java
+++ src/main/java/calculator/CalculatorModel.java
@@ -2,9 +2,9 @@ package calculator;
 
 public class CalculatorModel {
 
-    private Op op;
-    private double previous;
-    private double current;
+    private Op op = Op.NOOP;
+    private double previous = 0.0;
+    private double current = 0.0;
 
     public void setOp(Op op) {
         this.op = op;
